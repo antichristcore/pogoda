8e47961b829cebac24bdf31ba733c16eb8a0e236
tragedy <@spelowdim@yandex.ru>
2026-05-12 14:54:05 +0400

    forms + full models

---
+++ app/forms/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models.user import User
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired(), Length(min=3, max=80)])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Зарегистрироваться')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Это имя пользователя уже занято.')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Этот email уже зарегистрирован.')
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Текущий пароль', validators=[DataRequired()])
+    new_password = PasswordField('Новый пароль', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Подтвердите пароль', validators=[DataRequired(), EqualTo('new_password')])
+    submit = SubmitField('Сменить пароль')
+
+
+class CitySearchForm(FlaskForm):
+    city = StringField('Город', validators=[DataRequired(), Length(min=2, max=100)])
+    submit = SubmitField('Узнать погоду')
